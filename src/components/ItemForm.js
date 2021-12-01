125f1587a2ef4f24235d55aea96b42bc70b0564f
James Burns <jrburns96@aol.com>
2021-11-30 23:22:36 -0500

    Working except for the specific test requiring object to be passed back as an argument.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+
+function ItemForm({onItemFormSubmit, onItemCategoryChange, itemName, onItemNameChange, itemCategory}) {
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={itemName} onChange={onItemNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value = {itemCategory} onChange={onItemCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
